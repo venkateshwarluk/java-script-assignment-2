426982a896a1ecf60a1232b096645b5758ba9064
venkateshwarluk <ksrinuvenkatesh@gmail.com>
2016-11-07 14:56:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var myObj = {
+	name: "Raj",
+	age: "25 Years",
+	dateOfBirth: "05 JAN 1990",
+	placeOfBirth: "Bengalore"
+};
+
+console.log(myObj.name);
+console.log(myObj.age);
+console.log(myObj.dateOfBirth);
+console.log(myObj.placeOfBirth);
